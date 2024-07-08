a8d3fa105b1e7d8d42ed10cf82cf2b2b9ff2f084
Vishwajeet Ranaware <vishwajeetranaware105@gmail.com>
2024-07-09 00:11:52 +0530

    Product Card done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Navigation from "./customer/components/Navigation/Navigation.jsx";
-import HomePage from "./customer/Pages/HomePage/HomePage";
+// import HomePage from "./customer/Pages/HomePage/HomePage";
 import Footer from "./customer/components/FooterData/footer.jsx";
+import Product from "./customer/components/Product/Product.jsx";
 function App() {
   return (
     <div className="">
       <Navigation />
       <div>
-        <HomePage />
+        {/* <HomePage /> */}
+        <Product />
       </div>
       <div>
         <Footer></Footer>
